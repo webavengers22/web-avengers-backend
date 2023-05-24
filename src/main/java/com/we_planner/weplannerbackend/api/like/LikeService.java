d8c86aec5a668ee180b1c785bef72022832c7728
Haerim <71517226+kimhaerim@users.noreply.github.com>
2023-05-24 22:59:41 +0900

    좋아요 예외 처리 추가

--- src/main/java/com/we_planner/weplannerbackend/api/like/LikeService.java
+++ src/main/java/com/we_planner/weplannerbackend/api/like/LikeService.java
@@ -1,9 +1,17 @@
-
+@Service
 public class LikeService {
     private final LikeRepository likeRepository;
 
+    public Like findByPlanIdAndUserId(Long planId, Long userId){
+        return likeRepository.findByPlanIdAndUserId(plandId, userId);
+    }
+
     public boolean addLike(AddLikeRequestDto addLikeRequest){
-        // 이미 좋아요한 경우 예외 발생 필요
+        Like like = this.findByPlanIdAndUserId(addLikeRequest.planId, addLikeRequest.userId);
+
+        if(like){
+            throw new Error('같은 일정에 좋아요할 수 없습니다.');
+        }
         return likeRepository.save(addLikeRequest);
     }
 }
--- src/main/java/com/we_planner/weplannerbackend/api/like/repository/LikeRepository.java
+++ src/main/java/com/we_planner/weplannerbackend/api/like/repository/LikeRepository.java
@@ -1,3 +1,3 @@
 public interface LikeRepository extends JpaRepository<Like, Long>{
-    
+    Like findByPlanIdAndUserId(Long planId, Long userId);
 }
